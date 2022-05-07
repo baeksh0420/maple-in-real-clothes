a8a983fab47e89627cac3c2630cea00b3a6a6e48
SHyun <53278695+shyun46@users.noreply.github.com>
2022-05-07 18:48:59 +0900

    basic setting
    basic setting

---
+++ step/keypoint.py
@@ -0,0 +1,12 @@
+class Keypoint:
+    def method1(load_dir,save_dir,mode="none"):
+        """
+        방식 설명 : ~~ 에서 제공하는 툴을 사용한 수동 감지법 
+        """
+        print("- Keypoint Detection (method 1) : ~~ 에서 제공하는 툴을 사용한 수동 감지법")
+    
+    def method2(load_dir,save_dir,mode="none"):
+        """
+        방식 설명 : coco18을 사용한 자동 감지법
+        """
+        print("- Keypoint Detection (method 2) : coco18을 사용한 자동 감지법")
