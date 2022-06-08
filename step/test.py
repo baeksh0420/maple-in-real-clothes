17e2c2ba4a6e1e3fe9f99b195d2c9581f59562ae
baeksh0420 <baeksh.0420@gmail.com>
2022-06-08 20:34:42 +0900

    FIX test.py

--- step/test.py
+++ step/test.py
@@ -21,7 +21,7 @@ def get_opt():
     parser.add_argument('-b', '--batch-size', type=int, default=4)
     
     parser.add_argument("--dataroot", default = "data")
-    parser.add_argument("--datamode", default = "train")
+    parser.add_argument("--datamode", default = "test")
     parser.add_argument("--stage", default = "GMM")
     parser.add_argument("--data_list", default = "train_pairs.txt")
     parser.add_argument("--fine_width", type=int, default = 192)
@@ -42,8 +42,7 @@ def test_gmm(opt, test_loader, model, board):
     model.eval()
 
     base_name = os.path.basename(opt.checkpoint)
-    save_dir = "data/test" # os.path.join(opt.result_dir, base_name, opt.datamode)
-    print(save_dir) # 수정
+    save_dir = os.path.join(opt.dataroot, opt.datamode)
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     warp_cloth_dir = os.path.join(save_dir, 'warp-cloth')
@@ -52,7 +51,8 @@ def test_gmm(opt, test_loader, model, board):
     warp_mask_dir = os.path.join(save_dir, 'warp-mask')
     if not os.path.exists(warp_mask_dir):
         os.makedirs(warp_mask_dir)
-
+        
+        
     for step, inputs in enumerate(test_loader.data_loader):
         iter_start_time = time.time()
         
@@ -66,7 +66,7 @@ def test_gmm(opt, test_loader, model, board):
         cm = inputs['cloth_mask'].cuda()
         im_c =  inputs['parse_cloth'].cuda()
             
-        grid, theta = model(agnostic, c)
+        grid, theta = model(agnostic, c) # GMM 모델 후 아웃풋
         warped_cloth = F.grid_sample(c, grid, padding_mode='border')
         warped_mask = F.grid_sample(cm, grid, padding_mode='zeros')
         
@@ -75,7 +75,7 @@ def test_gmm(opt, test_loader, model, board):
                    [(warped_cloth+im)*0.5, im]]
         
         save_images(warped_cloth, c_names, warp_cloth_dir) 
-        save_images(warped_mask*2-1, c_names, warp_mask_dir) 
+        save_images(warped_mask*2-1, c_names, warp_mask_dir)
 
         if (step+1) % opt.display_count == 0:
             board_add_images(board, 'combine', visuals, step+1)
@@ -89,7 +89,7 @@ def test_tom(opt, test_loader, model, board):
     model.eval()
     
     base_name = os.path.basename(opt.checkpoint)
-    save_dir = os.path.join(opt.result_dir, base_name, opt.datamode)
+    save_dir = os.path.join(opt.dataroot, opt.datamode)
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     try_on_dir = os.path.join(save_dir, 'try-on')
@@ -99,7 +99,7 @@ def test_tom(opt, test_loader, model, board):
     for step, inputs in enumerate(test_loader.data_loader):
         iter_start_time = time.time()
         
-        im_names = inputs['im_name']
+        im_names = inputs['c_name'] #inputs['im_name']
         im = inputs['image'].cuda()
         im_pose = inputs['pose_image']
         im_h = inputs['head']
@@ -133,26 +133,36 @@ def main():
    
     # create dataset 
     train_dataset = CPDataset(opt)
+    print("*** train_dataset is DONE! ***")
 
     # create dataloader
     train_loader = CPDataLoader(opt, train_dataset)
+    print("*** train_loader is DONE! ***")
 
     # visualization
     if not os.path.exists(opt.tensorboard_dir):
         os.makedirs(opt.tensorboard_dir)
     board = SummaryWriter(log_dir = os.path.join(opt.tensorboard_dir, opt.name))
+    print("*** visualization code is DONE! ***")
    
     # create model & train
     if opt.stage == 'GMM':
         model = GMM(opt)
+        print("*** GMM model is Loaded! ***")
+        
         load_checkpoint(model, opt.checkpoint)
         with torch.no_grad():
             test_gmm(opt, train_loader, model, board)
+            print("*** test_gmm is DONE! ***")
+            
     elif opt.stage == 'TOM':
-        model = UnetGenerator(25, 4, 6, ngf=64, norm_layer=nn.InstanceNorm2d)
+        model = UnetGenerator(39, 4, 6, ngf=64, norm_layer=nn.InstanceNorm2d)
+        print("*** TOM model is Loaded! ***")
+        
         load_checkpoint(model, opt.checkpoint)
         with torch.no_grad():
             test_tom(opt, train_loader, model, board)
+            print("*** test_tom is DONE! ***")
     else:
         raise NotImplementedError('Model [%s] is not implemented' % opt.stage)
   
